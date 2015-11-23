cb35f4ff2a1a4f1df69d2e36b3233f93775784ae
ldabrowski <lukasz922@gmail.com>
2015-11-23 18:49:37 +0100

    Create insertion_sort.rb

---
+++ zadanie2/lib/insertion_sort.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby -w
+def insertion_sort(arr)
+  (1...arr.length).each do |i|
+    current_val = arr[i]
+    j = i - 1
+    
+    while j >= 0 && arr[j] > current_val
+      arr[j + 1] = arr[j]
+      j -= 1
+    end
+    arr[j + 1] = current_val
+  end
+  
+  arr
+end
